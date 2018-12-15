71b6bab3dfe3ab5bf2d616d95ba450f519d55584
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2018-12-15 17:51:12 +0530

    next commit

--- src/screens/CreateResponse.js
+++ src/screens/CreateResponse.js
@@ -7,10 +7,11 @@ import {
   ScrollView,
   Image,
   TextInput,
-  Dimensions
+  Dimensions,
+  ImageBackground
 } from "react-native";
 import { Card, Avatar } from "react-native-elements";
-import { PrimaryButton } from "../components/CommonUI";
+import { PrimaryButton, Separator } from "../components/CommonUI";
 import * as Api from "../services/Api";
 import update from "immutability-helper";
 import { Ionicons } from "@expo/vector-icons";
@@ -32,22 +33,23 @@ export default class CreateResponse extends React.Component {
     super(props);
     let mockImage = {
       cancelled: false,
-      height: 1200,
+      height: 420,
       type: "image",
       uri:
-        "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540pranav.fullstack%252Fapp/ImagePicker/557e48d1-6da8-4b9c-a0cd-b0f4cb1f32fe.jpg",
-      width: 750
+        "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540pranav.fullstack%252Fapp/ImagePicker/84fd529a-9686-4b34-9f5f-fb02ac518712.jpg",
+      width: 480
     };
     //image: this.props.navigation.getParam("image", null)
     let { height, width } = Dimensions.get("window");
     this.state = {
-      image: this.props.navigation.getParam("image", null),
+      image: mockImage,
       comment: null,
       placeholderComment: "Optional comment...",
       UUserId: "5c10b94950726f47d9c1a626",
       height: height,
       width: width
     };
+    console.log(this.state.image);
   }
   render() {
     let { image } = this.state;
@@ -56,33 +58,68 @@ export default class CreateResponse extends React.Component {
     // console.log(this.getImageDimensions(image, screen));
     return (
       <ScrollView>
-        <View style={{ backgroundColor: "#424242" }}>
+        <View
+          style={{ backgroundColor: "#424242", width: width, height: height }}
+        >
           {this.state.image && (
-            <Image
+            <ImageBackground
+              style={{
+                height: height,
+                width: width,
+                alignItems: "flex-end",
+                flexDirection: "row"
+              }}
               source={{ uri: image.uri }}
-              style={{ flex: 1, height: height, width: width }}
-            />
+            >
+              <View
+                style={{
+                  height: 60,
+                  width: width,
+                  backgroundColor: "#424242",
+                  opacity: 0.9,
+                  padding: 8
+                }}
+              >
+                <Text style={{ color: "#42A5F5", fontSize: 16 }}>
+                  Reply: Where is Mowgli
+                </Text>
+                <Text style={{ color: "#F5F5F5", fontSize: 14 }}>
+                  Posted by : Anamika
+                </Text>
+              </View>
+            </ImageBackground>
           )}
         </View>
-        <View>
+        <View style={{ marginLeft: 8, marginRight: 8 }}>
           <TextInput
             style={{
               height: 45,
               borderBottomWidth: 1,
               fontSize: 18,
               borderBottomColor: "#BDBDBD",
-              marginLeft: 8,
-              marginRight: 8
+              marginBottom: 8
             }}
             placeholder={this.state.placeholderComment}
             onChangeText={text => this.setState({ comment: text })}
             value={this.state.comment}
           />
-          <PrimaryButton
-            title="Post"
-            onPress={this.postResponse.bind(this)}
-            buttonStyle={{ backgroundColor: "#42A5F5" }}
-          />
+          <View style={{marginBottom:8}}>
+            <Text style={{ color: "#616161" }}>View more options</Text>
+          </View>
+          <Separator/>
+          <View style={{ alignItems: "center" }}>
+            <PrimaryButton
+              title="Post Reply"
+              onPress={this.postResponse.bind(this)}
+              containerStyle={{ width: 150 }}
+              buttonStyle={{
+                backgroundColor: "#42A5F5",
+                borderRadius: 16,
+                width: "100%",
+                height:35
+              }}
+            />
+          </View>
         </View>
       </ScrollView>
     );
