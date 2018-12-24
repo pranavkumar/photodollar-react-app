53e9ec46f13a3f07a2b8c9b6e8e932aa249b5211
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2018-12-24 13:07:49 +0530

    next commit

---
+++ src/components/RequestActions.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
+// import { Card, Avatar } from "react-native-elements";
+import { PrimaryButton } from "../components/CommonUI";
+import { Ionicons } from "@expo/vector-icons";
+
+export default class RequestActions extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <PrimaryButton
+          title="Forward"
+          style={{ fontFamily: "regular" }}
+          containerStyle={{ paddingLeft: 0 }}
+          type="outline"
+          buttonStyle={{ fontFamily: "regular" }}
+        />
+        <PrimaryButton
+          title="Expect"
+          onPress={this.props.onExpect}
+          style={{ fontFamily: "regular" }}
+          containerStyle={{ paddingLeft: 0 }}
+          type="outline"
+          buttonStyle={{ fontFamily: "regular" }}
+        />
+        <PrimaryButton
+          style={{ fontFamily: "light" }}
+          onPress={this.props.onReply}
+          title="Reply"
+          containerStyle={{ paddingLeft: 0 }}
+          buttonStyle={{ backgroundColor: "#448AFF", fontFamily: "regular" }}
+        />
+      </View>
+    );
+  }
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -14,6 +14,7 @@ import * as Util from "../utils";
 import update from "immutability-helper";
 import { Ionicons } from "@expo/vector-icons";
 import { ImagePicker, Permissions, Contacts } from "expo";
+import RequestActions from "../components/RequestActions";
 
 export default class Home extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -32,12 +33,14 @@ export default class Home extends React.Component {
   }
 
   render() {
-    let {fontLoaded} = this.state;
-    if(!fontLoaded) return null;
+    let { fontLoaded } = this.state;
+    if (!fontLoaded) return null;
     return (
       <View style={{ padding: 0 }}>
-        <View style={{height:60,backgroundColor:"#448AFF",padding:10}}>
-          <Text style={{fontFamily:"light",fontSize:22,color:"white"}}>KyaScene</Text>
+        <View style={{ height: 60, backgroundColor: "#448AFF", padding: 16 }}>
+          <Text style={{ fontFamily: "light", fontSize: 22, color: "white" }}>
+            KyaScene
+          </Text>
         </View>
         <ScrollView>
           <FlatList
@@ -56,52 +59,29 @@ export default class Home extends React.Component {
           <View
             style={{ flexDirection: "column", marginLeft: 8, marginBottom: 8 }}
           >
-            <Text style={{ fontSize: 16, fontWeight: "normal", fontFamily:"regular" }}>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "normal",
+                fontFamily: "regular"
+              }}
+            >
               {item.title}
             </Text>
           </View>
         </View>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "flex-start",
-            alignItems: "center"
-          }}
+
+        <RequestActions
+          onReply={this.handleCreateReply.bind(this, item)}
+          onExpect={this.handleToggleExpect.bind(this, item)}
         />
-        {item.UResponses &&
-          item.UResponses.length > 0 &&
-          this.renderResponses(item.UResponses)}
-        <View />
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-          <PrimaryButton
-            title="Forward"
-            containerStyle={{ paddingLeft: 0 }}
-            type="outline"
-          />
-          <PrimaryButton
-            title="Expect"
-            containerStyle={{ paddingLeft: 0 }}
-            type="outline"
-          />
-          <PrimaryButton
-            onPress={this.handleCreateReply.bind(this, item)}
-            title="Reply"
-            containerStyle={{ paddingLeft: 0 }}
-            buttonStyle={{ backgroundColor: "#42A5F5" }}
-          />
-        </View>
       </Card>
     );
   }
   renderResponses(items) {
     return (
       <FlatList
+        style={{ backgroundColor: "red" }}
         keyExtractor={(item, index) => index.toString()}
         data={items}
         renderItem={({ item }) => this.renderResponse(item)}
@@ -110,7 +90,7 @@ export default class Home extends React.Component {
   }
   renderResponse(item) {
     return (
-      <View style={{ marginBottom: 16 }}>
+      <View style={{ marginBottom: 32 }}>
         <Image
           style={{ width: "100%", height: 100 }}
           source={{
@@ -120,10 +100,9 @@ export default class Home extends React.Component {
       </View>
     );
   }
-  componentWillMount = async()=>{
-    console.log("gonna mount");
+  componentWillMount = async () => {
     await this.loadFonts();
-  }
+  };
   componentDidMount() {
     Api.getRequests()
       .then(({ status, data }) => {
@@ -140,6 +119,9 @@ export default class Home extends React.Component {
   handleCreateReply = async request => {
     console.log(`handling reply...${request.id}`);
     this.props.navigation.navigate("CameraReply", { request: request });
+  };
+  handleToggleExpect = async request => {
+    console.log(`handling expect...${request.id}`);
   };
 }
 
