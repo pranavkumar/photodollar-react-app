032d0eecf8f05a93f9417dc069ba0055d635a66d
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2018-12-26 18:57:17 +0530

    next commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,7 +5,8 @@ import {
   View,
   FlatList,
   ScrollView,
-  Image
+  Image,
+  TouchableOpacity
 } from "react-native";
 import { Card, Avatar } from "react-native-elements";
 import { PrimaryButton } from "../components/CommonUI";
@@ -14,7 +15,13 @@ import * as Util from "../utils";
 import * as Utils from "../services/Utils";
 
 import update from "immutability-helper";
-import { Ionicons } from "@expo/vector-icons";
+import {
+  Ionicons,
+  SimpleLineIcons,
+  Feather,
+  EvilIcons,
+  AntDesign
+} from "@expo/vector-icons";
 import { ImagePicker, Permissions, Contacts } from "expo";
 import RequestActions from "../components/RequestActions";
 
@@ -43,20 +50,83 @@ export default class Home extends React.Component {
     let { fontLoaded } = this.state;
     if (!fontLoaded) return null;
     return (
-      <View style={{ padding: 0 }}>
+      <View style={{ padding: 0, flexDirection: "column", flex: 1 }}>
         <View style={{ height: 60, backgroundColor: "#448AFF", padding: 16 }}>
           <Text style={{ fontFamily: "light", fontSize: 22, color: "white" }}>
             KyaScene
           </Text>
         </View>
-        <ScrollView style={{ marginBottom: 100 }}>
-          <FlatList
-            extraData={this.state.refresh}
-            keyExtractor={(item, index) => index.toString()}
-            data={this.state.coverageRequests}
-            renderItem={({ item, index }) => this.renderRequest(item, index)}
-          />
-        </ScrollView>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          extraData={this.state.refresh}
+          keyExtractor={(item, index) => index.toString()}
+          data={this.state.coverageRequests}
+          renderItem={({ item, index }) => this.renderRequest(item, index)}
+        />
+        <View
+          style={{
+            alignSelf: "flex-end",
+            height: 50,
+            width: "100%",
+            flexDirection: "row"
+          }}
+        >
+          <View
+            style={{
+              width: "33%",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <TouchableOpacity
+              onPress={this.gotoProfile.bind(this)}
+              style={{
+                width: "100%",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <SimpleLineIcons name="user" size={28} color="#448AFF" />
+            </TouchableOpacity>
+          </View>
+
+          <View
+            style={{
+              width: "33%",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <TouchableOpacity
+              onPress={this.gotoRequest.bind(this)}
+              style={{
+                width: "100%",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <SimpleLineIcons name="plus" size={32} color="#448AFF" />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              width: "33%",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <TouchableOpacity
+              onPress={this.gotoSearch.bind(this)}
+              style={{
+                width: "100%",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <AntDesign name="search1" size={32} color="#448AFF" />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
@@ -144,8 +214,17 @@ export default class Home extends React.Component {
     this.props.navigation.navigate("CameraReply", { request: request });
   };
   handleForward = async request => {
-    this.props.navigation.navigate("ForwardRequest", { URequestId: request.id });
+    this.props.navigation.navigate("ForwardRequest", {
+      URequestId: request.id
+    });
   };
+  gotoProfile() {
+    this.props.navigation.navigate("UserProfile");
+  }
+  gotoRequest() {
+    this.props.navigation.navigate("CreateRequest");
+  }
+  gotoSearch() {}
 }
 
 const styles = StyleSheet.create({
